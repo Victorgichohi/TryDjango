50eaf865a57b8efec9486b30eef23761fbfc86f0
Victorgichohi <victorgichohi48@gmail.com>
2015-11-21 22:42:32 +0300

    added the form validation code

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -6,3 +6,12 @@ class SignUPForm(forms.ModelForm):
     class meta:
         model=SignUP
         fields = ["full_name","email"]
+    def clean_email(self):
+        email =self.cleaned_data.get('email')
+        email_base, provider = email.split("@")
+        domain,extension = provider.split('.')
+        if not domain == 'gmail':
+            raise forms.ValidationError("please make sure to use a gmail account")
+        if not extension == '.com':
+            raise forms.ValidationError("please enter the correct email adress")
+        return email
