e9e2c20201944fac77f71321d3c4b625b9635c31
Victorgichohi <victorgichohi48@gmail.com>
2015-11-22 01:01:37 +0300

    added a thankyou note to notify someone after they signup

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -7,14 +7,22 @@ def home(request):
     title = "testing meen"
     form = SignUPForm(request.POST or None)
 
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.save()
-        # print instace.email
-        print instance.timestamp
-
     context = {
     "template_title": title,
     "form" : form
     }
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        full_name =form.cleaned_data.get("full_name")
+        if not full_name:
+            full_name="New full name"
+        instance.full_name=full_name
+        instance.save()
+
+        context={
+        "title":"thank you"
+        }
+
+
     return render(request, "home.html",context)
